8e6c39ca0923dbea1d149c105d0464018d38fc7e
Gaurav Ashish <71788323+rghvgrv@users.noreply.github.com>
2024-12-31 00:02:52 +0530

    Added Code for Amazon

--- app.py
+++ app.py
@@ -1,25 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
+import urllib
 
 # URL of the product page
-url = "https://www.flipkart.com/benq-mx560-4000-lm-1-speaker-remote-controller-dlp-20000-1-high-contrast-ratio-dual-hdmi-usb-a-upto-15000-hrs-extra-long-lamp-life-10w-speaker-3d-capable-xga-business-education-projector/p/itmc2c43606e806a?pid=PROGVP6Z52ZGGGZA&lid=LSTPROGVP6Z52ZGGGZAJ03DQK&marketplace=FLIPKART&q=benq&store=6bo%2Ftia&srno=s_1_1&otracker=search&otracker1=search&fm=Search&iid=da213a09-c689-46b2-a87a-c6268dd482bb.PROGVP6Z52ZGGGZA.SEARCH&ppt=sp&ppn=sp&ssid=oc96p7988w0000001735497552049&qH=eacf6d16adf5c83d"
+url = "https://www.flipkart.com/cmf-nothing-100-w-power-gan-5-3-port-mobile-charger-detachable-cable/p/itm2f0f45bd04629?pid=ACCH4H7AJHNEYNXA&lid=LSTACCH4H7AJHNEYNXAFF4GNF&marketplace=FLIPKART&q=cmf+charger&store=tyy%2F4mr%2Ftp2&srno=s_1_4&otracker=AS_QueryStore_OrganicAutoSuggest_1_5_na_na_na&otracker1=AS_QueryStore_OrganicAutoSuggest_1_5_na_na_na&fm=search-autosuggest&iid=f179953f-388b-48e4-8ce3-c70e78455de0.ACCH4H7AJHNEYNXA.SEARCH&ppt=sp&ppn=sp&ssid=p4iffod0dc0000001735569958496&qH=f36109ce0fe857e2"
 
 # Set headers to mimic a browser
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0 Safari/537.36"
-}
+# headers = {
+#     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0 Safari/537.36"
+# }
 
 # Make a GET request
-response = requests.get(url, headers=headers)
+response = requests.get(url)
 
 # Check if the request was successful
 if response.status_code == 200:
     
     soup = BeautifulSoup(response.text,'html.parser')
 
-    title = soup.find('h1').text.strip()
+    title = soup.find('h1').text.strip().replace("/", "")
+    encoded_query = urllib.parse.quote_plus(title)
 
-    print(f"Product Name : {title}")
+    print(f"Product Name : {encoded_query}")
 
     pricing = soup.find("div", {"class": "hl05eU"}).text.strip()
 
@@ -35,3 +37,25 @@ if response.status_code == 200:
 
 else:
     print(f"Failed to fetch the page. Status code: {response.status_code}")
+
+
+# Format search URL for Amazon
+amazon_url = f"https://www.amazon.in/s/ref=nb_sb_noss?field-keywords={encoded_query}"
+
+print(amazon_url)
+
+response = requests.get(amazon_url)
+
+# Parse the results
+if response.status_code == 200:
+    soup = BeautifulSoup(response.content, "html.parser")  
+    # print(soup)  
+    # Extract product titles from the search results
+    products = soup.find_all("a", class_="a-link-normal s-no-hover s-underline-text s-underline-link-text s-link-style a-text-normal")
+    print(products[0].get_text())
+    ref = products[0].get('href')
+    print(f"https://www.amazon.in{ref}")
+    # for product in products[:5]:  # Limit to first 5 results
+    #     print(product.get_text())
+else:
+    print(f"Failed to fetch Amazon search results. Status code: {response.status_code}")
