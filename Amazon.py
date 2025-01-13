91ce9bfbabd483210c4f1c43ea7396e1284d0123
Gaurav Ashish <71788323+rghvgrv@users.noreply.github.com>
2025-01-13 22:08:34 +0530

    Add Seperate Code

--- Amazon.py
+++ Amazon.py
@@ -1,12 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
-import time
 import json
 from fuzzywuzzy import fuzz
 
-from main import fetch_amazon_product_details, get_flipkart_product_details
-
-def search_flipkart(product_name):
+def Amazon_Flipkart(product_name):
     # Limit the product name to the first two or three words
     limited_product_name = ' '.join(product_name.split()[:3])
     search_url = f"https://www.flipkart.com/search?q={requests.utils.quote(limited_product_name)}&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off"
@@ -39,8 +36,7 @@ def search_flipkart(product_name):
                     if best_match:
                         product_link = best_match.get('url')
                         if product_link:
-                            title,price = get_flipkart_product_details(product_link)
-                            return title, price
+                            return product_link
                     print("Matching product link not found on Flipkart.")
                     return None
                 else:
@@ -56,18 +52,39 @@ def search_flipkart(product_name):
         print(f"Failed to fetch the page. Status code: {response.status_code}")
         return None
 
+def Amazon_ProductDetails(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Referer': 'https://www.google.com/'
+    }
+    
+    session = requests.Session()
+    response = session.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        title_section = soup.find('span', id='productTitle')
+        if title_section:
+            product_title = title_section.text.strip()
+        else:
+            print("Title section not found.")
+            return None, None
+        
+        price_section = soup.find('span', id="tp_price_block_total_price_ww")
+        if price_section:
+            product_price = price_section.text.strip()
+        else:
+            print("Price not found.")
+            product_price = None
+        
+        return product_title, product_price
+    else:
+        print(f"Failed to fetch the page. Status code: {response.status_code}")
+        return None, None
+    
+
 # Example usage
 if __name__ == "__main__":
     amazon_url = "https://www.amazon.in/Portable-Protection-Compatible-Warranty-SDSSDE30-1T00-G26/dp/B0C5JQ68FY/ref=pd_ci_mcx_mh_mcx_views_0_title?pd_rd_w=ICHk5&content-id=amzn1.sym.529d03fa-575b-4f2b-a4d6-0c02eabf0a7e%3Aamzn1.symc.45dc5f4c-d617-4dba-aa26-2cadef3da899&pf_rd_p=529d03fa-575b-4f2b-a4d6-0c02eabf0a7e&pf_rd_r=7CT4127R875GRSYR6S08&pd_rd_wg=NCtCU&pd_rd_r=99719cc8-5c21-460e-a62c-8a06c5caab0d&pd_rd_i=B0C5JQ68FY&th=1"
-
-    amzon_title, amzn_price = fetch_amazon_product_details(amazon_url)
-    print(f"Amazon Product Name -----> {amzon_title}\n")
-    print(f"Amazon Product Price -----> {amzn_price}\n")
-
-    if amzon_title:
-        flpkrt_title,flpkrt_price = search_flipkart(amzon_title)
-        
-    print(f"Flipkart Product Name ----> {flpkrt_title}\n")
-    print(f"Flipkart Product Price ----> {flpkrt_price}\n")
-    # Introduce a delay to avoid being flagged as a bot
-    time.sleep(2)
--- Flipkart.py
+++ Flipkart.py
@@ -3,9 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import urllib
 
-from main import fetch_amazon_product_details, get_flipkart_product_details
-
-def fetch_amazon_product(title):
+def Flipkart_Amazon(title):
     
     encoded_query = urllib.parse.quote_plus(title)
     amazon_url = f"https://www.amazon.in/s/ref=nb_sb_noss?field-keywords={encoded_query}"
@@ -17,8 +15,9 @@ def fetch_amazon_product(title):
         'Referer': 'https://www.google.com/'
     }
 
-    response = requests.get(amazon_url, headers=headers)
     time.sleep(2)
+
+    response = requests.get(amazon_url, headers=headers)
 
     # Parse the results
     if response.status_code == 200:
@@ -30,21 +29,25 @@ def fetch_amazon_product(title):
                 continue
             else:
                 link=f"https://www.amazon.in{ref}"
-                amzn_title,amzn_price = fetch_amazon_product_details(link)
-                return amzn_title,amzn_price
+                return link
     else:
         print(f"Failed to fetch Amazon search results. Status code: {response.status_code}")
+
+def Flipkart_ProductDetails(url):
+
+    response = requests.get(url)
+    if response.status_code == 200:     
+        soup = BeautifulSoup(response.text,'html.parser')
+        title = soup.find('h1').text.strip().replace("/", "").replace("[", "").replace("]", "")
+        pricing = soup.find("div", {"class": "hl05eU"}).text.strip()
+        cleaned_price = pricing.lstrip("₹")
+        price_parts = cleaned_price.split("₹")
+        price = price_parts[0].replace(",", "")
+        return title,price
+    
+    else:
+        print(f"Failed to fetch the page. Status code: {response.status_code}")
 
 if __name__ == "__main__":
     # URL of the product page
     productLink = "https://www.flipkart.com/logitech-mx-master-3s-wireless-touch-mouse/p/itm271ade01ff274?pid=ACCGFPFCAFGREFHZ&lid=LSTACCGFPFCAFGREFHZMWXUSH&marketplace=FLIPKART&q=mx+master+3s&store=6bo%2Fai3%2F2ay&srno=s_1_1&otracker=AS_QueryStore_OrganicAutoSuggest_1_12_na_na_ps&otracker1=AS_QueryStore_OrganicAutoSuggest_1_12_na_na_ps&fm=search-autosuggest&iid=847c1894-9948-43d8-86be-a7fe149a41a0.ACCGFPFCAFGREFHZ.SEARCH&ppt=sp&ppn=sp&ssid=nq91tj9xkw0000001736691420158&qH=44b100c8bdb752b1"
-
-    flpkrt_title,flpkrt_price = get_flipkart_product_details(productLink)
-    print(f"Flipkart Product Name ----> {flpkrt_title}\n")
-    print(f"Flipkart Product Price ----> {flpkrt_price}\n")
-
-    amzon_title,amzn_price = fetch_amazon_product(flpkrt_title)
-    print(f"Amazon Product Name -----> {amzon_title}\n")
-    print(f"Amazon Product Price -----> {amzn_price}\n")
-
-    time.sleep(2)
